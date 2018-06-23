2f5c40ecd2107920edc1eb7ad388d7436fdbc2ce
Simon Wörpel <simon.woerpel@medienrevolte.de>
2018-06-23 16:35:44 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+genesapi clean ./src defaults.yaml --target-dir ./cleaned --dtypes ./build/dtypes.json
+genesapi transform ./cleaned --dtypes ./build/dtypes.json > ./build/db.csv
+genesapi build_tree ./build/db.csv ./build/keys.json > ./build/tree.json
+
