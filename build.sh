75112493298aa46cd7433262f14e07ea0b82b448
Simon Wörpel <simon.woerpel@medienrevolte.de>
2018-06-24 15:57:54 +0200

    Add fix for Hamburg & Berlin

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 genesapi clean ./src defaults.yaml --target-dir ./cleaned --dtypes ./build/dtypes.json
 genesapi transform ./cleaned --dtypes ./build/dtypes.json > ./build/db.csv
-genesapi build_tree ./build/db.csv ./build/keys.json > ./build/tree.json
+genesapi build_tree ./build/db.csv ./build/keys.json --fix ./fix_be_hh.yaml > ./build/tree.json
 
